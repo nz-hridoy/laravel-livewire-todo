92476351fcd6889296a9ce10089e33048fceb892
nz-hridoy <nayeemuzaman05@gmail.com>
2023-06-20 07:25:36 +0600

    layouts setup

---
+++ resources/views/livewire/index-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Livewire TODO App</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\IndexComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', IndexComponent::class)->name('app.index');
