4fd008ecc936925f1431c4b9833e97481ddd3fb4
nz-hridoy <nayeemuzaman05@gmail.com>
2023-06-20 08:12:47 +0600

    user interface design

--- resources/views/livewire/index-component.blade.php
+++ resources/views/livewire/index-component.blade.php
@@ -1,3 +1,218 @@
 <div>
-    <h1>Livewire TODO App</h1>
+    <style>
+        .list-scroll::-webkit-scrollbar-track
+        {
+            background-color: #ffffff;
+        }
+
+        .list-scroll::-webkit-scrollbar
+        {
+            width: 3px;
+            background-color: #ffffff;
+        }
+
+        .list-scroll::-webkit-scrollbar-thumb
+        {
+            background-color: #cecece;
+        }
+    </style>
+    <div class="container-sm">
+        <div class="row justify-content-center mt-5">
+            <div class="col-lg-6 col-md-10">
+
+                <div class="card">
+                    <div class="card-header pb-1">
+                        <h5><strong>NzCoding - ToDo App</strong></h5>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Add new task">
+                                <button class="btn btn-dark input-group-append">
+                                    <i class="fa fa-plus-circle"></i>
+                                </button>
+                            </div>
+                        </form>
+
+                        <hr>
+
+                        <ul class="list-group list-scroll" style="user-select: none; height: 67vh; overflow-y: scroll;">
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center mb-2" style="border: 1px solid #DEE2E6;">
+                                <span style="display: flex;">
+                                    <input type="checkbox" id="item_1" style="margin-right: 10px;">
+                                    <label for="item_1" class="">Cras justo odio lor</label>
+                                </span>
+                                <a href="#" class="text-danger"><i class="fa fa-times"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="card-footer text-center pt-1">
+                        <small class="text-muted">Coded by <b>NzCodinG</b></small>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/layouts/base.blade.php
+++ resources/views/livewire/layouts/base.blade.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>NzCoding - Livewire ToDo</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     @livewireStyles
 </head>
-
 <body>
 
 
     {{ $slot }}
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 
     @livewireScripts
 
 </body>
-
 </html>
